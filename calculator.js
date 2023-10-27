ab685a04937a17b4193c7528798b9710b55cd614
Hyunsoo-k <red_panda119@naver.com>
2023-10-27 10:55:52 +0900

    reset to first

--- calculator.js
+++ calculator.js
@@ -1,7 +1,17 @@
+// basic calculator
+
 function add(a, b){
     return a+b;
 }
 
 function sub(a, b){
     return a-b;
+}
+
+function multifly(a, b){
+    return a*b;
+}
+
+function sayHello() {
+    console.log('hello');
 }
